82cdb14ea3dfafe9bb41875f307fb61dd4381e57
Joseph Avila <avilajoseph2021@gmail.com>
2025-04-04 16:25:08 -0500

    creación del componente Login

---
+++ src/auth/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function LoginScreen(){
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
