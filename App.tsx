aaef77562c009cdf90fb213d9a90bb747a6a2b15
AvilaJoseph <avilajoseph2021@gmail.com>
2025-04-05 11:18:47 -0500

    Correciones de img de fondo en el RegisterSuccessful

--- App.tsx
+++ App.tsx
@@ -4,10 +4,11 @@ import LoginScreen from './src/auth/Login/LoginScreen'
 import RegisterScreen from './src/auth/Register/RegisterScreen';
 import LoginFormScreen from './src/auth/Login/LoginFormScreen';
 import LoginSucessful from './src/auth/Login/LoginSucessful';
+import RegisterSucessfull from './src/auth/Register/RegisterSucessfull';
 
 export default function App() {
   return (
-    <LoginSucessful></LoginSucessful>
+    <RegisterSucessfull></RegisterSucessfull>
   );
 }
 
